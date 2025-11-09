e6cab57a47ca762fdfe208e1029fff16557052d1
王鑫123wangxin <1418667580@qq.com>
2025-11-09 16:41:45 +0800

    wx, feat: 模拟一个恋爱agent

---
+++ src/main/java/top/wjstar/xinaiagent/demo/rag/LoveAppDocumentLoader.java
@@ -0,0 +1,12 @@
+package top.wjstar.xinaiagent.demo.rag;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author wxvirus
+ */
+@Component
+@Slf4j
+public class LoveAppDocumentLoader {
+}
